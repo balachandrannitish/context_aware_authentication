564af2727d57ccdfc37d9d078d26fe03d77a4b23
balachandrannitish <nitish.balachandran@gmail.com>
2018-02-27 04:20:36 -0500

    Added globals to configure time range for samples

--- integration.py
+++ integration.py
@@ -1,6 +1,9 @@
-import numpy
+import numpy as np
 
 BATCH_TIME_PERIOD_IN_SECONDS = 60.0
+SKIP_LINE_LIST = ["accelerometer.txt","All values are in SI units (m/s^2).","http://developer.android.com/guide/topics/sensors/sensors_overview.html","elapsed-time-system elapsed-time-sensor x y z"]
+FROM_SECOND = 20.0
+TO_SECOND = 380.0
 
 # variables to build INPUT_FILE_PATH
 sensor = "accelerometer"
@@ -9,73 +12,56 @@ position = "carsidepocket"    # example values include "stacked", "sidebyside", 
 phone_owner = "SAURABH"  # example values include "NITISH", "SAURABH"
 file_format = ".txt"
 
-INPUT_FILE_PATH = "Data/" + sensor + "_" + activity + "_" + position + "_" + phone_owner + file_format
+INPUT_FILE_SAURABH = sensor + "_" + activity + "_" + position + "_" + "SAURABH" + file_format
+INPUT_FILE_NITISH = sensor + "_" + activity + "_" + position + "_" + "NITISH"  + file_format
 
-SKIP_LINE1 = "accelerometer.txt"
-SKIP_LINE2 = "All values are in SI units (m/s^2)."
-SKIP_LINE3 = "http://developer.android.com/guide/topics/sensors/sensors_overview.html"
-SKIP_LINE4 = "elapsed-time-system elapsed-time-sensor x y z"
-SKIP_LINE_ARRAY = [SKIP_LINE1,SKIP_LINE2,SKIP_LINE3,SKIP_LINE4]
+def parse_file_and_compute_integrals(file_path):
+	"""
+	Calculates the integrals (z^2 * t) for every batch and returns a list
+	"""
+	with open(file_path,"r") as input_file:
 
-def parse_file():
+		# intialize batch counter and "current" lists
+		batch_counter = 1
+		z_current = []  # z_current is the set of z^2 values that constitute a single batch
+		t_current = []  # t_current is the set of t values that constitute a single batch
+		integral_list = []
 
-	t_to_z = {}
-
-	with open(INPUT_FILE_PATH,"r") as input_file:
-
+		# iterate through file
 		for line in input_file:
+			
 			line = line.strip()
-			print line
-			if line:
-
-				if line in SKIP_LINE_ARRAY:
-					print "SKIPPING"
-				
-				else:
+			if line and line not in SKIP_LINE_LIST:
+					# extract t,x,y,z from the line
 					line_list = line.split(" ")
-
-					sensor_time = float(line_list[1])
+					t = float(line_list[1])
 					x = float(line_list[2])
 					y = float(line_list[3])
 					z = float(line_list[4])
 
-					t_to_z[sensor_time] = z
+					if t>FROM_SECOND:
+						z_current.append(z*z)
+						t_current.append(t)
 
-	return t_to_z
+					if t>(FROM_SECOND + batch_counter*BATCH_TIME_PERIOD_IN_SECONDS):
+						integral_value = np.trapz(z_current,t_current,axis=-1)
+						integral_list.append(integral_value)
+						batch_counter = batch_counter + 1
+						z_current = []
+						t_current = []
 
+					if t>TO_SECOND:
+						break
+
+		integral_value = np.trapz(z_current,t_current,axis=-1)
+		integral_list.append(integral_value)
+
+		return integral_list
 
 if __name__ == '__main__':
-	
-	t_to_z_dict = parse_file()
 
-	#z_batch_array = []
-	#t_batch_array = []
-
-	z_list = []
-	t_list = []
-
-	multiplier = 0
-
-	for time in t_to_z_dict.keys():
-
-		z = t_to_z_dict[time]
-
-		multiplier = multiplier + 1
-
-		z_list = []
-		t_list = []
-
-		while time < multiplier*BATCH_TIME_PERIOD_IN_SECONDS:
-
-			z_list.append(z)
-			t_list.append(time)
-
-			print "Multiplier is: " + str(multiplier)
-			integral_value = np.trapz(z,time,axis=-1)
-
-			print "Integral value is: " + str(integral_value)
-			d = raw_input("Press any key")
-
+	print parse_file_and_compute_integrals(INPUT_FILE_SAURABH)
+	print parse_file_and_compute_integrals(INPUT_FILE_NITISH)
 
 
 
